1f9b69c7e72cf48e68984a224f162e5ca3aab4c5
Kaio Rodrigues Saravy <kaiorodriguessaravy@gmail.com>
2023-01-09 15:01:44 -0400

    Adicionando configurações iniciais

---
+++ src/main/java/br/com/digix/estagix/EstagixApplication.java
@@ -0,0 +1,13 @@
+package br.com.digix.estagix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstagixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstagixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/digix/estagix/EstagixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digix.estagix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstagixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
